79468b2e5d52def375038d4de9d13335d8cd1eeb
JohnyTea <61454541+JohnyTea@users.noreply.github.com>
2021-03-10 14:35:11 +0100

    Add files via upload

---
+++ BreakPointTest/BreakPointTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BreakPointTest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zgadnij o jakiej liczbie myślę!");
+            RandomNumber number = new RandomNumber();
+            number.generateNumber();
+
+            double guess = double.Parse(Console.ReadLine());
+            double check = number.checkAnswear(guess);
+            while (check != 0)
+            { 
+                
+                if (check == -1)
+                {
+                    Console.WriteLine("To za mało");
+                }
+                else if (check == 1)
+                {
+                    Console.WriteLine("To za dużo");
+                }
+                guess = double.Parse(Console.ReadLine());
+                check = number.checkAnswear(guess);
+            }
+            Console.WriteLine("Brawo Zgadłeś liczbę!");
+        }
+    }
+}
---
+++ BreakPointTest/BreakPointTest/RandomNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreakPointTest
+{
+    class RandomNumber
+    {
+        private double secretNumber;
+
+        public void generateNumber()
+        {
+            Random rand = new Random();
+            secretNumber = rand.NextDouble()*100;
+        }
+
+        public int checkAnswear(double number)
+        {
+            if (number > secretNumber)
+            {
+                return 1;
+            }
+            else if (number < secretNumber)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
